b3730066cb336b4b4a162306b98e1d4687ddb69f
Juan Nestares <juan.nestares@travelhx.com>
2025-09-18 15:57:31 +0200

    fix4

--- sits/draw.js
+++ sits/draw.js
@@ -3,14 +3,16 @@ function deploy(parentNode,obj) {
 	const btn=document.createElement("input")
 	const tpe=document.createAttribute("type")
 	tpe.value='submit'
-	btn.setAttribute(tpe)
+	btn.setAttributeNode(tpe)
 	const style=document.createAttribute("style")
 	style.value='left:'+obj.x+';top:'+obj.y+';rotate:'+obj.rotate+';'
 	btn.setAttributeNode(style)
 	const val=document.createAttribute("value")
 	val.value=obj.status
+	btn.setAttributeNode(val)
 	const nme=document.createAttribute("name")
 	nme.value=obj.name
+	btn.setAttributeNode(nme)
 	parentNode.appendChild(btn)
 }
 
