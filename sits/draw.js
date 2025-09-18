7f86bce144cff877b0b248a7e822282f0d21ebbd
Juan Nestares <juan.nestares@travelhx.com>
2025-09-18 15:29:12 +0200

    sits poc

---
+++ sits/draw.js
@@ -0,0 +1,16 @@
+function deploy(obj) {
+	const map=document.getElementById("map")
+	const btn=document.createElement("input")
+	const tpe=document.createAttribute("type")
+	tpe.value='submit'
+	btn.setAttribute(tpe)
+	const style=document.createAttribute("style")
+	style.value='left:'+obj.x+';top:'+obj.y+';rotate:'+obj.rotate+';'
+	btn.setAttributeNode(style)
+	const val=document.createAttribute("value")
+	val.value=obj.status
+	const nme=document.createAttribute("name")
+	nme.value=obj.name
+	map.appendChild(btn)
+}
+
